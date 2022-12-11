67d18ead398ac3dff9de83960172671ae4151833
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-10 16:17:57 -0800

    Update web_table1.py

--- web_table1.py
+++ web_table1.py
@@ -7,3 +7,7 @@ driver = webdriver.Chrome(service=serv_obj)
 
 driver.get("https://testautomationpractice.blogspot.com/")
 driver.maximize_window()
+
+# Count the number of rows and columns
+num_of_rows = len(driver.find_elements(By.XPATH, "//table[@name='BookTable']//tr"))
+num_of_columns = len(driver.find_elements(By.XPATH, "//table[@name='BookTable']//tr/th"))
