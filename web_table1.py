14df0af1870cd497e0c9ee0a3aa3a6fb4687e568
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-10 16:25:30 -0800

    Update web_table1.py

--- web_table1.py
+++ web_table1.py
@@ -11,3 +11,14 @@ driver.maximize_window()
 # Count the number of rows and columns
 num_of_rows = len(driver.find_elements(By.XPATH, "//table[@name='BookTable']//tr"))
 num_of_columns = len(driver.find_elements(By.XPATH, "//table[@name='BookTable']//tr/th"))
+
+print(num_of_rows)  # 7, including the headers (in the 1st row)
+print(num_of_columns) # 4
+
+# Read specific row and columns data
+
+
+# Read all the rows and columns data
+
+
+# List books based on a condition
