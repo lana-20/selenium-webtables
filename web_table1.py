66bf59fec112a0217162ec26f4bf9a2773ddb951
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-21 11:50:46 -0800

    Update web_table1.py

--- web_table1.py
+++ web_table1.py
@@ -16,11 +16,16 @@ print(num_of_rows)  # 7, including the headers (in the 1st row)
 print(num_of_columns) # 4
 
 # Read specific row and columns data
-
 data = driver.find_element(By.XPATH, "//table[@name='BookTable']/tbody/tr[5]/td[1]").text
 print(data) # Master In Selenium
 
 # Read all the rows and columns data
+print("Printing all the rows and columns data")
 
+for r in range(2, num_of_rows + 1):
+	for c in range(1, num_of_columns + 1):
+		data = driver.find_element(By.XPATH, "//table[@name='BookTable']/tbody/tr["+str(r)+"]/td["+str(c)+"]").text
+	print(data, end="   ")
+print()
 
 # List books based on a condition
