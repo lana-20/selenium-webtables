f5b08a779290198eb81417cae3f0f94d8e0eccef
Lana Begunova <70295997+lana-20@users.noreply.github.com>
2022-12-21 20:48:41 -0800

    Update webtable_dynamic.py

--- webtable_dynamic.py
+++ webtable_dynamic.py
@@ -3,7 +3,6 @@
 # https://bonigarcia.dev/webdrivermanager/
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
